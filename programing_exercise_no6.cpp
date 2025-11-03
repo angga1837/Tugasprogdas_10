01ad8fae87899925addf5d79caebe30351f4cf46
Jangga_MKS <67214645+angga1837@users.noreply.github.com>
2025-11-03 10:26:45 +0700

    Add files via upload

---
+++ programing_exercise_no6.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct studentType{
+    string studentFname;
+    string studentLname;
+    int testScore;
+    char grade;
+};
+studentType classlist[20];
+
+char assigngrade(int score){
+    if(score <= 100 && score >95) return 'A';
+    else if(score <=95 && score > 90) return 'B';
+    else if(score <= 90 && score >80) return 'C';
+    else if (score <= 80 && score >70) return 'D';
+    else return 'E';
+}
+void readtoarray(int n){
+    for (int i = 0; i<n; i++){
+        cout << "masukkan nama murid ke-" << i+1 << endl;
+        cin >> classlist[i].studentFname >> classlist[i].studentLname;
+        cout << "masukkan score murid ke-" << i+1 << endl;
+        cin >> classlist[i].testScore;
+        classlist[i].grade = assigngrade(classlist[i].testScore);
+    }
+    cout << "terimakasih sudah input data, berikut nama-nama siswa yang kamu input:"<< endl;
+    for (int i = 0; i<n; i++) cout << classlist[i].studentLname << ", " <<classlist[i].studentFname << endl;
+}
+
+void studenthighest(int indexmaks){
+    cout << "Siswa tersebut adalah: " << classlist[indexmaks].studentLname << ", " <<classlist[indexmaks].studentFname;
+}
+
+void highestscore(int n){
+    int max = classlist[0].testScore;
+    int indexmaks = 0;
+    for (int i = 0; i < n; i++ ){
+        if (classlist[i].testScore > max){
+            max = classlist[i].testScore;
+            indexmaks = i;
+        }
+    }
+    cout << "Nilai tertinggi adalah: " << max << "dengan grade: " << classlist[indexmaks].grade << endl;
+    studenthighest(indexmaks);
+}
+int main(){
+    readtoarray(20);
+    highestscore(20);
+}
